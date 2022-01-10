72a61e58b5b8478f0484e987a21b608e3c91a87b
Sathnindu Kottage <45420836+sathninduk@users.noreply.github.com>
2022-01-11 00:25:49 +0530

    fe build

--- frontend/src/reducers/authReducer.js
+++ frontend/src/reducers/authReducer.js
@@ -8,7 +8,7 @@ const initialState = {
   loading: false
 };
 
-export default function(state = initialState, action) {
+export default function foo(state = initialState, action) {
   switch (action.type) {
     case SET_CURRENT_USER:
       return {
